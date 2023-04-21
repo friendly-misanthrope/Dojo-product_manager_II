e1f1c35895745e242a0c50ee09e6705d6a7f3ee3
NickBarkerDev <nickthedevguy@gmail.com>
2023-04-21 16:01:18 -0700

    imported dependencies, set up lifted state, made API call with useEffect

--- react-client/src/components/DisplayProducts.jsx
+++ react-client/src/components/DisplayProducts.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import { useEffect } from "react";
+import axios from 'axios'
+import { Link } from "react-router-dom";
 
-const DisplayProducts = () => {
+const DisplayProducts = (props) => {
+
+  const {allProducts, setAllProducts} = props
+
+  useEffect(() => {
+    axios.get('http://172.19.219.207:800/api/products')
+      .then((res) => {
+        setAllProducts(res.data)
+      })
+      .catch((err) => console.log(err))
+  }, [setAllProducts])
+
   return (
-    <div>
+    <div className="display-products">
       
     </div>
   );
